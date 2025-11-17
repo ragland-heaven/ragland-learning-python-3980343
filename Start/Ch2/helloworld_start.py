17416334db4e3e1a73095d9bf80e636d758d124a
ragland-heaven <hragland2@hawkmail.hccfl.edu>
2025-11-17 19:32:05 +0000

    Complete exercises w/o loops and functions Ch2

--- Start/Ch2/helloworld_start.py
+++ Start/Ch2/helloworld_start.py
@@ -1,2 +1,5 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for HelloWorld
+print ("Hello World")
+name = input ("What is yor name? ")
+print ("Nice to meet you", name)
--- Start/Ch2/loops_start.py
+++ Start/Ch2/loops_start.py
@@ -5,11 +5,19 @@
 x = 0
 
 # define a while loop
+# while x < 5:
+#   print(x)
+#   x = x + 1
 
+# answer = input("Should I stop?")
+# while answer != "yes":
+#   print(answer)
+#   answer = input("Should I stop?")
 
 # define a for loop
-
-
+days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"]
+for d in days:
+  print(d)
 # use a for loop over a collection
 
 
